002cfe64626977c8a8728371a1e914e478bddc03
xiaolei@globalroam.com <xiaolei@globalroam.com>
2015-11-04 11:38:32 +0800

    version 1.01

---
+++ app/src/main/java/com/u3/dontdistraction/MyApplication.java
@@ -0,0 +1,16 @@
+package com.u3.dontdistraction;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Administrator on 2015/11/4.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
