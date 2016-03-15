aa4942dbb7492e2ada84f5752b7fd737e6406822
u3 <u3shadow@gmail.com>
2016-03-15 11:42:06 +0800

    add penlatymsg test

---
+++ app/src/main/java/test/com/u3/dontdistraction/other/PenaltyMessageTest.java
@@ -0,0 +1,18 @@
+package test.com.u3.dontdistraction.other;
+
+import com.u3.dontdistraction.other.PenaltyMessage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ${U3} on 2016/3/15.
+ */
+public class PenaltyMessageTest {
+    @Test
+    public void get_penalty_message(){
+        PenaltyMessage penaltyMessage = new PenaltyMessage();
+       String msg =  penaltyMessage.getMessage();
+        Assert.assertNotNull(msg);
+    }
+}
